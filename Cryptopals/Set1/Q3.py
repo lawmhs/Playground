de3519d3dd0e238279dfaf3f872af93b2f29d837
lawmhs <matthew.hs.law@hotmail.com>
2017-06-21 17:59:00 -0500

    Added something about binascii

--- Cryptopals/Set1/Q3.py
+++ Cryptopals/Set1/Q3.py
@@ -4,18 +4,9 @@ import scipy as np
 import argparse
 import codecs
 import Q2 as q2
+import binascii
 
-# First let's test the Q2 function
-str1 = '1c0111001f010100061a024b53535009181c'
-str2 = '686974207468652062756c6c277320657965'
+encrypted = '1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736'
+# encrypted is encoded by a single character
 
-a = codecs.decode(str1, 'hex_codec')
-b = codecs.decode(str2, 'hex_codec')
-
-c = q2.xor(a, b)
-print(c)
-
-d = codecs.encode(c, 'hex_codec')
-print(d)
-
-
+print(binascii.unhexlify(encrypted))
