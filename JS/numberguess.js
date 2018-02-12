39105e31c011e32b720e41e17d0c838d0b3ff062
lawmhs <matthew.hs.law@hotmail.com>
2018-02-12 15:45:42 -0800

    a

--- JS/numberguess.js
+++ JS/numberguess.js
@@ -1,9 +1,1 @@
-var prompt = require('prompt');
 
-prompt.start();
-
-prompt.get(['username', 'email'], function (err, result) {
-	console.log('command-line input received:');
-	console.log(' username: ' + result.username);
-	console.log(' email: ' + result.email);
-	});
