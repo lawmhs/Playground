6c1b097488a876e7617535f4992dab1bd0d31a28
lawmhs <matthew.hs.law@hotmail.com>
2018-12-05 10:47:50 -0800

    deleted extra file

--- rust/TextEdit/src/main.rs
+++ rust/TextEdit/src/main.rs
@@ -1,3 +1,18 @@
+pub struct TextEditor {
+    test: String // Private member variable
+}
+
+impl TextEditor {
+    pub fn new() -> TextEditor {
+        TextEditor{ test: String::new() }
+    }
+
+    // modify text
+    pub fn add_char(&mut self, ch : char) {
+        self.text.push(ch);
+    }
+}
+
 fn main() {
     println!("Hello, world!");
 }
