928650c88803211b81c57d1023eed82ce96ded34
lawmhs <matthew.hs.law@hotmail.com>
2019-02-11 21:51:40 -0800

    Added the palindrome functionality

--- rust/cli/src/main.rs
+++ rust/cli/src/main.rs
@@ -16,24 +16,32 @@ fn main() {
         .subcommand(SubCommand::with_name("palindrome")
             .version("1.0")
             .about("checks whether input string is a palindrome")
-            .arg_from_usage("-s, --string, 'the string to be checked'"))
+            .arg(Arg::with_name("input")
+                .required(true)
+                .index(1)
+                .help("The string to be checked"))
+            .arg_from_usage("-s, --string 'argument is the string to be checked'"))
         .get_matches();
 
-    match matches.subcommand_name() {
-        Some("quit") => {
+    // match consumes so we need to get everything out at once
+    // if you only get the name you've already partially moved the data
+    // so ownership has changed
+    match matches.subcommand() {
+        ("quit", Some(_quit_matches)) => {
             println!("Goodbye!");
             process::exit(0);
         },
-        Some("palindrome") => {
-            if palindrome(matches.value_of("input")
-                .expect("there should be an argument")) {
-                println!("The string {} is a palindrome!", matches.value_of("input").unwrap());
+        ("palindrome", Some(palindrome_matches)) => {
+            let strng = palindrome_matches.value_of("input")
+                .expect("There should be an argument"); // moves it into strng
+            if palindrome(strng) {
+                println!("The string {} is a palindrome!", strng);
             }
             else {
-                println!("The string {} is not a palindrome!", matches.value_of("input").unwrap());
+                println!("The string {} is not a palindrome!", strng);
             }
         }
-        None => println!("No subcommand was used!"),
+        ("", None) => println!("No subcommand was used!"),
         _ => println!("Invalid subcommand was used!")
 
     }
@@ -41,5 +49,12 @@ fn main() {
 }
 
 fn palindrome(s: &str) -> bool {
-    true
+
+    s.char_indices().zip(
+        s.char_indices().rev())
+        .take_while(|&((first_count, _) , (last_count, _))| {first_count < last_count})
+        .all(|((_, first_char), (_, last_char))| {
+            first_char.to_ascii_lowercase() == last_char.to_ascii_lowercase()
+        })
+
 }
