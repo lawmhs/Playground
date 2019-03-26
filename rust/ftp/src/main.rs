39b25c2091e18beca90f656afefafb1da3675585
lawmhs <matthew.hs.law@hotmail.com>
2019-03-26 15:17:35 -0700

    Revert "Revert "Revert "Figured out how to read the passive socket information and create the appropriate socket"""
    This reverts commit 81809bc19715fa136bee2190b03f7b8eda8e794a.

--- rust/ftp/src/main.rs
+++ rust/ftp/src/main.rs
@@ -6,9 +6,7 @@ use std::process::exit;
 use std::env;
 use std::net::{Ipv4Addr, SocketAddrV4, TcpStream, TcpListener};
 use std::process::Command;
-use std::str::FromStr;
 use bufstream::BufStream;
-use regex::Regex;
 
 /// main loop of the code
 fn main() -> std::io::Result<()> {
@@ -21,7 +19,7 @@ fn main() -> std::io::Result<()> {
     let port = &args[2].parse::<i32>().expect("Should be a number here");
 
     // create an address
-    let socket_addr = SocketAddrV4::new(Ipv4Addr::new(142, 103, 6, 49), 21); // ftp.cs.ubc.ca
+    let socket_addr = SocketAddrV4::new(Ipv4Addr::new(127, 0, 0, 1), 60000);
 
     // then create a socket
     let mut stream = match TcpStream::connect(socket_addr) {
@@ -68,62 +66,17 @@ fn main() -> std::io::Result<()> {
                 println!("< {}", line.trim());
             }
             "feat" => {
-
                 buf.get_mut().write_all("FEAT\r\n".as_ref());
 
                 // not the most elegant way to do things
                 line.clear();
                 buf.read_line(&mut line);
-
                 while !line.to_lowercase().contains("end") {
                     println!("< {}", line.trim());
                     line.clear();
                     buf.read_line(&mut line);
                 }
                 println!("< {}", line.trim()); // messy way to do things
-            }
-            "ls" => {
-
-                // switch to passive mode
-                buf.get_mut().write_all("PASV\r\n".as_ref());
-                line.clear();
-                buf.read_line(&mut line); // only outputs one line
-
-                // line will be something like:
-                // 227 Entering Passive Mode (142, 103, 6, 49, 107, 180)
-                // need to use regex to recognize the numbers?
-
-                println!("{}", line.trim()); // line is printing properly
-
-                // need to open a second connection
-                let mut ip_array = [0, 0, 0, 0];
-                let pasv_addr: Ipv4Addr; // this can be uninitialized because we're guaranteed to initialize this after the for
-                let mut port = 0; // this must be initialized because the assignment is inside an if else, which the compiler thinks
-                // we may or may not hit
-
-
-                let re = Regex::new(r"(\d+),(\d+),(\d+),(\d+),(\d+),(\d+)").expect("should be a valid regex pattern");
-                let caps = re.captures(line.trim()).expect("Should have a capture group from entering passive for ls command"); // should only have one capture group
-
-                for i in 0..5 {
-                    if i < 4 {
-                        // the first element in the captures is not usable as a number
-                        ip_array[i] = caps.get(i + 1).unwrap().as_str().parse::<u8>().expect("need valid ipv4 addr segment");
-                    } else {
-                        let big = caps.get(5).unwrap().as_str().parse::<u16>().expect("first part of port number invalid") * 256;
-                        let small = caps.get(6).unwrap().as_str().parse::<u16>().expect("second part of port numebr invalid");
-                        port = big + small;
-                    }
-
-                }
-
-                pasv_addr = Ipv4Addr::new(ip_array[0], ip_array[1], ip_array[2], ip_array[3]);
-                println!("IP Address: {}, Using Port: {}", pasv_addr, port);
-
-                // initialize a new connection:
-                let pasv_socket = SocketAddrV4::new(pasv_addr, port);
-
-
             }
             "quit" => {
                 println!("> Goodbye!");
