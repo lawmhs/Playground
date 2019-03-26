81809bc19715fa136bee2190b03f7b8eda8e794a
lawmhs <matthew.hs.law@hotmail.com>
2019-03-26 15:15:45 -0700

    Revert "Revert "Figured out how to read the passive socket information and create the appropriate socket""
    This reverts commit 66f9ab9634d9d36aafee6ed30ea076de48b818c3.

---
+++ rust/ftp/target/debug/deps/aho_corasick-667bc9939851ad4c.d
@@ -0,0 +1,7 @@
+/home/user/Documents/Projects/Playground/rust/ftp/target/debug/deps/libaho_corasick-667bc9939851ad4c.rlib: /home/user/.cargo/registry/src/github.com-1ecc6299db9ec823/aho-corasick-0.6.10/src/lib.rs /home/user/.cargo/registry/src/github.com-1ecc6299db9ec823/aho-corasick-0.6.10/src/autiter.rs /home/user/.cargo/registry/src/github.com-1ecc6299db9ec823/aho-corasick-0.6.10/src/full.rs
+
+/home/user/Documents/Projects/Playground/rust/ftp/target/debug/deps/aho_corasick-667bc9939851ad4c.d: /home/user/.cargo/registry/src/github.com-1ecc6299db9ec823/aho-corasick-0.6.10/src/lib.rs /home/user/.cargo/registry/src/github.com-1ecc6299db9ec823/aho-corasick-0.6.10/src/autiter.rs /home/user/.cargo/registry/src/github.com-1ecc6299db9ec823/aho-corasick-0.6.10/src/full.rs
+
+/home/user/.cargo/registry/src/github.com-1ecc6299db9ec823/aho-corasick-0.6.10/src/lib.rs:
+/home/user/.cargo/registry/src/github.com-1ecc6299db9ec823/aho-corasick-0.6.10/src/autiter.rs:
+/home/user/.cargo/registry/src/github.com-1ecc6299db9ec823/aho-corasick-0.6.10/src/full.rs:
